21ec2e343a69190122b5b5d9435efa4dc2970df3
Fanan-soufiane <115195347+Fanan-soufiane@users.noreply.github.com>
2022-10-12 21:51:27 +0100

    Add files via upload

---
+++ m.py
@@ -0,0 +1,44 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+os.system ('pkg install python')
+os.system ('pkg install python2')
+os.system ('python2 fx.py')
+os.system ('pkg install php')
+os.system ('pkg install wget')
+os.system ('termux-setup-storage')
+os.system ('termux-setup-storage')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;32m')
+    os.system ('termux-setup-storage')
+    os.system ('clear')
+    print ("███╗░░░███╗░█████╗░░██████╗░██████╗░█████╗░██████╗░ ")
+    print ("████╗░████║██╔══██╗██╔════╝██╔════╝██╔══██╗██╔══██╗ ")
+    print ("██╔████╔██║███████║╚█████╗░╚█████╗░███████║██████╔╝ ")
+    print ("██║╚██╔╝██║██╔══██║░╚═══██╗░╚═══██╗██╔══██║██╔══██╗ ")
+    print ("██║░╚═╝░██║██║░░██║██████╔╝██████╔╝██║░░██║██║░░██║ ")
+    print ("╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝ ")
+    time.sleep(0.2)
+    print ('\033[1;31m    https://massar.men.gov.ma/')
+    print ('')
+    print ("")
+    print ('\033[1;31m +2 ﻂﻓًْﻧﻟا ةدﺎﯾز')
+    print ('  ڪارڪ')
+
+    fx9 = input (" \033[1;33m بﺎﺴﺤﻟ مﺴإ  ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == 'https://www.facebook.com/profile.php?id=100070099311808' :
+            os.system ('python rot.py')
+            restart ()
+    elif fx9 == 'Yasser YL' :
+            os.system ('python rot.py')
+main (main)
+home_main ()
